bb2ab93bf252861e4641fbdfcf11ddaf073b861d
trvrgrhm <trvrgrhm@gmail.com>
2020-12-14 14:34:25 -0700

    first command line reading and writing

---
+++ week1/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a word.");
+            string word = Console.ReadLine();
+            Console.WriteLine("You said \""+word+"\"!!!");
+        }
+    }
+}
