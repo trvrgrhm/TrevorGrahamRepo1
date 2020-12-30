00f7da487428689700ca237f3176ff3ca6800316
trvrgrhm <trvrgrhm@gmail.com>
2020-12-29 23:18:23 -0700

    adds order funcionality

--- P0_TrevorGraham/StoreApplication/StoreMain/Logic/StoreSession.cs
+++ P0_TrevorGraham/StoreApplication/StoreMain/Logic/StoreSession.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using StoreApplication.Models;
@@ -13,13 +14,15 @@ namespace StoreApplication.Logic
         /// <returns></returns>
         StoreDbContext db = new StoreDbContext();
 
-        //session variables
+        #region session variables
         /// <summary>
         /// Customer that is currently logged in; null if nobody is logged in
         /// </summary>
         Customer currentCustomer = null;
         Location currentLocation = null;
+        Inventory currentProduct = null;
         List<OrderLine> cart = new List<OrderLine>();
+        #endregion
 
         /// <summary>
         /// Attempts find Customer with given username in database. If successful, assigns the Customer as the signed in user and returns true; otherwise, returns false
@@ -43,6 +46,83 @@ namespace StoreApplication.Logic
             currentLocation = db.Locations.SingleOrDefault(x => x.Name == storeName);
             return currentLocation != null;
         }
+        public bool AttemptChooseProduct(string productName){
+            currentProduct = currentLocation.InventoryItems.SingleOrDefault(x => x.Product.ProductName == productName); 
+            return currentProduct!=null;
+        }
+
+        public bool AttemptAddToCart(int quantity){
+            OrderLine cartLine =new OrderLine(){
+                Quantity = quantity,
+                Inventory = currentProduct,
+            };
+            cart.Add(cartLine);
+            return true;
+
+        }
+
+        /// <summary>
+        /// Attempts to add cart to db as order; Any quantities that are too high are dropped
+        /// </summary>
+        /// <returns>total cost of cart items</returns>
+        public bool AttemptCheckout(){
+            if(!IsLoggedIn()||!ProductIsChosen()||!StoreIsChosen()){
+                return false;
+            }
+            //decrement inventories in db
+            List<OrderLine> removeThese = new List<OrderLine>();
+            foreach(OrderLine cartLine in cart){
+                if(cartLine.Inventory.Quantity - cartLine.Quantity>=0){
+                    cartLine.Inventory.Quantity -= cartLine.Quantity;
+                }
+                else{
+                    removeThese.Add(cartLine);
+                }
+            }
+            if(removeThese.Any()){
+                foreach(OrderLine line in removeThese){
+                    cart.Remove(line);
+                }
+            }
+            if(cart.Any()){
+                Order newOrder = new Order(){
+                    Date = DateTime.Now,
+                    Customer = currentCustomer,
+                    Lines = cart
+                };
+                db.Orders.Add(newOrder);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        public double CartTotal(){
+            return cart.Select(x=>x.Inventory.Product.Price).Sum();
+        }
+        public double OrderTotal(int orderId){
+            Order order = db.Orders.SingleOrDefault(x=>x.OrderId==orderId);
+            if(order!=null)
+            return order.Lines.Select(x=>x.Inventory.Product.Price).Sum();
+            return 0.0;
+        }
+        public List<int> GetOrderIds(string customerName, string location){
+            List<int> orderIds = new List<int>();
+            foreach(Order order in db.Orders){
+                if(order.Customer.Username == customerName && order.Lines[0].Inventory.Location.Name == location){
+                    orderIds.Add(order.OrderId);
+                }
+            }
+            return orderIds;
+
+        }
+
+        public bool RemoveAllItemsFromCart(){
+            cart = null;
+            return cart == null;
+        }
+
+
+
 
 
         #region add to db
@@ -56,16 +136,16 @@ namespace StoreApplication.Logic
             }
             if(db.Products.Count()<1){
                 for(int i = 0;i<3;i++){
-                    AddLocationToDb("Product "+i);
+                    AddProductToDb("Product "+i,i+1,"Something Special");
                 }
             }
             //restock stores
-            foreach(Location store in db.Locations){
-                if(store.InventoryItems.Count()<1){
-                    foreach(Product product in db.Products){
+            foreach(Location store in db.Locations.ToList()){
+                 if(store.InventoryItems.Count()<1){
+                    foreach(Product product in db.Products.ToList()){
                         AddInventoryItem(store,product,10);
                     }
-                }
+                 }
             }
         }
 
@@ -154,11 +234,18 @@ namespace StoreApplication.Logic
             return currentCustomer !=null;
         }
         /// <summary>
-        /// Returns true if the customer has chosen a store; returns fals if not
+        /// Returns true if the customer has chosen a store; returns false if not
         /// </summary>
         /// <returns></returns>
         public bool StoreIsChosen(){
             return currentLocation != null;
+        }
+        /// <summary>
+        /// Returns true if the customer has chosen a product; returns false if not
+        /// </summary>
+        /// <returns></returns>
+        public bool ProductIsChosen(){
+            return currentProduct != null;
         }
         /// <summary>
         /// Returns true if the cart is empty; returns false if the cart contains any items.
@@ -201,6 +288,9 @@ namespace StoreApplication.Logic
                 return null;
             }
         }
+        public string GetCurrentProduct(){
+            return currentProduct.Product.ProductName;
+        }
         /// <summary>
         /// Returns a list containing all of the store names in the db
         /// </summary>
@@ -211,6 +301,51 @@ namespace StoreApplication.Logic
                 storeNames.Add(store.Name);
             }
             return storeNames;
+        }
+        /// <summary>
+        /// Returns a list with the inventory of the current store.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetCurrentProductNames(){
+            List<string> products = new List<string>();
+            foreach(Inventory inventory in currentLocation.InventoryItems){
+                products.Add(inventory.Product.ProductName);
+            }
+            return products;
+        }
+        /// <summary>
+        /// Returns the price as a string for the product in the current store location
+        /// </summary>
+        /// <param name="prductName"></param>
+        /// <returns></returns>
+        public string GetProductPrice(string prductName){
+            if(StoreIsChosen()){
+                return currentLocation.InventoryItems.SingleOrDefault(x => x.Product.ProductName ==prductName).Product.Price.ToString();
+            }
+            return "not in inventory";
+        }
+        /// <summary>
+        /// Returns the quantity as a string for the product in the current store location
+        /// </summary>
+        /// <param name="prductName"></param>
+        /// <returns></returns>
+        public int GetProductQuantity(string prductName){
+            if(StoreIsChosen()){
+                return currentLocation.InventoryItems.SingleOrDefault(x => x.Product.ProductName ==prductName).Quantity;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns a list with the inventory of the current store.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetCurrentInventory(){
+            List<string> products = new List<string>();
+            foreach(Inventory inventory in currentLocation.InventoryItems){
+                products.Add($"\n{inventory.Product.ProductName}\nPrice: ${inventory.Product.Price}\n{inventory.Quantity} left in stock\n");
+            }
+            return products;
         }
         #endregion
     }
